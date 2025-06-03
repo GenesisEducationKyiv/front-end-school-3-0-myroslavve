08b07b16ff4d27bffd7c1154c453909f33c5cc8b
Myroslav <verstyukm@gmail.com>
2025-06-04 01:27:13 +0300

    Remove unnecessary JSDoc

--- src/lib/utils/input.ts
+++ src/lib/utils/input.ts
@@ -1,12 +1,5 @@
 import { AnyFunction } from "./types";
 
-/**
- * Creates a debounced function that delays invoking the provided function
- * until after the specified wait time has elapsed since the last time it was invoked.
- * 
- * @param func The function to debounce
- * @param wait The number of milliseconds to delay
- */
 export function debounce<T extends AnyFunction>(func: T, wait: number) {
     let timeout: NodeJS.Timeout | undefined;
   
