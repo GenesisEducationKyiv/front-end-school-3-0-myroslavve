63d6ce450b7dff71aec6822d4ee4a7c1080a4c7f
Myroslav <verstyukm@gmail.com>
2025-06-29 00:34:44 +0300

    Wrap tracks in suspense boundary to prevent next error with `useSearchParams`

--- packages/client/src/app/page.tsx
+++ packages/client/src/app/page.tsx
@@ -2,12 +2,16 @@
 
 import { Toaster } from "sonner";
 import { Tracks } from "./_components/tracks";
+import { Suspense } from "react";
+import Spinner from "@/components/ui/spinner";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start lg:w-full">
-        <Tracks />
+        <Suspense fallback={<Spinner />}>
+          <Tracks />
+        </Suspense>
         <Toaster />
       </main>
     </div>
