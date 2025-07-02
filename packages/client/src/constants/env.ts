4ea67d1716fc4d086eb0ad8405afe7a4c5f46561
Myroslav <verstyukm@gmail.com>
2025-07-02 17:14:00 +0300

    Add env type and remove defaults

--- packages/client/src/constants/env.ts
+++ packages/client/src/constants/env.ts
@@ -1,4 +1,9 @@
+export type Env = {
+  apiUrl: string;
+  storageUrl: string;
+}
+
 export const env = {
-  apiUrl: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000",
-  storageUrl: process.env.NEXT_PUBLIC_STORAGE_URL || "http://localhost:8000/api/files",
-} as const;
+  apiUrl: process.env.NEXT_PUBLIC_API_URL!,
+  storageUrl: process.env.NEXT_PUBLIC_STORAGE_URL!,
+} satisfies Env;
