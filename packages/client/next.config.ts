a4d3facc37b16c6941445ddc1c81417121af4b28
Myroslav <verstyukm@gmail.com>
2025-07-07 00:23:03 +0300

    Try to optimize tree shaking

--- packages/client/next.config.ts
+++ packages/client/next.config.ts
@@ -11,10 +11,49 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
   webpack: (config, { dev }) => {
     if (!dev) {
       config.devtool = 'hidden-source-map';
     }
+    
+    config.optimization = {
+      ...config.optimization,
+      moduleIds: 'deterministic',
+      chunkIds: 'deterministic',
+      splitChunks: {
+        chunks: 'all',
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            priority: 10,
+            chunks: 'all',
+          },
+          audioPlayer: {
+            test: /[\\/]node_modules[\\/]react-h5-audio-player[\\/]/,
+            name: 'audio-player',
+            priority: 20,
+            chunks: 'all',
+          },
+          tanstack: {
+            test: /[\\/]node_modules[\\/]@tanstack[\\/]/,
+            name: 'tanstack',
+            priority: 15,
+            chunks: 'all',
+          },
+          radix: {
+            test: /[\\/]node_modules[\\/]@radix-ui[\\/]/,
+            name: 'radix',
+            priority: 15,
+            chunks: 'all',
+          },
+        },
+      },
+    };
+
     return config;
   },
 };
