8e0255320824e378e1cbc21ae0e5d75c152ca8d0
Myroslav <verstyukm@gmail.com>
2025-07-07 00:11:38 +0300

    Preconnect to API and google fonts

--- packages/client/src/app/layout.tsx
+++ packages/client/src/app/layout.tsx
@@ -21,11 +21,30 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+  const storageUrl = process.env.NEXT_PUBLIC_STORAGE_URL;
+
   return (
     <html lang="en">
       <head>
         <meta name="title" content="Music Player" />
         <meta name="description" content="Music Player" />
+        
+        {apiUrl && (
+          <>
+            <link rel="preconnect" href={new URL(apiUrl).origin} crossOrigin="anonymous" />
+            <link rel="dns-prefetch" href={new URL(apiUrl).origin} />
+          </>
+        )}
+        {storageUrl && (
+          <>
+            <link rel="preconnect" href={new URL(storageUrl).origin} crossOrigin="anonymous" />
+            <link rel="dns-prefetch" href={new URL(storageUrl).origin} />
+          </>
+        )}
+        
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
